530678a52409e22b13d204bb8494c7f33174c997
TingFeng <ftingsjtu@gmail.com>
2017-11-22 14:57:21 -0500

    Update twoSum.py

--- twoSum.py
+++ twoSum.py
@@ -1,5 +1,9 @@
 
 """
+
+LeetCode 1: TwoSum
+https://leetcode.com/problems/two-sum/description/
+
 #Problem:
 
 Given an array of integers, return indices of the two numbers such that they add up to a specific target.
