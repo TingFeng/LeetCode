81b95ed8f4bebb22654673c3337908908268e690
TingFeng <ftingsjtu@gmail.com>
2017-11-29 16:19:39 -0500

    Create addTwoNumbers.py

---
+++ addTwoNumbers.py
@@ -0,0 +1,58 @@
+
+"""
+LeetCode Problem #2
+
+You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
+
+You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+Output: 7 -> 0 -> 8
+
+Solution:
+Hint: python pointers
+
+"""
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def addTwoNumbers(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        Sum=l1.val+l2.val
+        Result=ListNode(Sum%10)
+        flag=Sum/10
+        Temp=Result
+        
+        while l1.next and l2.next:
+            l1=l1.next
+            l2=l2.next
+            Sum=l1.val+l2.val+flag
+            Temp.next=ListNode(Sum%10)
+            flag=Sum/10
+            Temp=Temp.next
+        while l1.next:
+            l1=l1.next
+            Sum=l1.val+flag
+            Temp.next=ListNode(Sum%10)
+            flag=Sum/10
+            Temp=Temp.next
+        while l2.next:
+            l2=l2.next
+            Sum=l2.val+flag
+            Temp.next=ListNode(Sum%10)
+            flag=Sum/10
+            Temp=Temp.next
+            
+        if flag>0:
+            Temp.next=ListNode(flag)
+            
+        return Result
